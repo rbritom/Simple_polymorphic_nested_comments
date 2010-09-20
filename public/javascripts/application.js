cc8655a5e72ed422b088cabb7db973a2fd7c9615
Radhames Brito <rbritom@gmail.com>
2010-09-20 08:59:30 -0400

    fixed a bug in the application.js

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,4 +1,4 @@
 $($(".comments_panel a").live("click", function() {
-$("#comment_content").val("@"+ $(this).attr("comment_author"+"\n"));
+$("#comment_content").val("@"+ $(this).attr("comment_author"));
 $("#comment_parent_id").val($(this).attr("comment_id"));
 }));
