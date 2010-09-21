911ec5e71971cb852b5c808961f37381dce218c4
Radhames Brito <rbritom@gmail.com>
2010-09-21 08:39:53 -0400

    fixed more typos and errors in the comments of the comment action

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -40,7 +40,7 @@ class CommentsController < ApplicationController
     # Read create first
     #
     # The only mayor diference between this and the create
-    # action is that this time im passing ad inclure option
+    # action is that this time im passing ad include option,
     # all this does is tell active record that when it fetches
     # the article or proyect it should also fetch its comments
     # in the same query. The include option is passed to the
@@ -67,8 +67,8 @@ class CommentsController < ApplicationController
     @comment.destroy
     flash[:notice] = "Successfully destroyed comment."
     # :back is an alias to env[:HTTP_REFERER]
-    # and holds the page that send to this action
-    # redirecting to :back  saves us from need to
+    # and holds the page that sent us to this action
+    # redirecting to :back  saves us having from to
     # use context_object to figure out how to
     # redirect to the parent
     #
@@ -79,15 +79,14 @@ class CommentsController < ApplicationController
   private
 
    # this function here is the reason why :constraint => {:context_type => "articles"} was added to
-   # routes file, everytime the params hash comes from withthing the comments has, the routes adds
-   # the context type to the params hash, so i access that value by reading params[:constraint] the outer
-   # value of the hash and add [:context_type] to access the inner value or the hash, that is because constrains
-   # is a hash it self and can have a group of hash, out of which [:context_type] is one, and you can pull
-   # the value of a hash inside a hash the way you pull the value of a nested array.  In each context the value
-   # return if am accesing comments from inside the articles or the proyects, so those are the 2 values i can ge
-   # but they are only string, and are in plural, so i make it singular with singularice and make a constan with
-   # contantize, that why the values become Article and Proyect, the name of the models! so now they are model
-   # name and i call find on them, but i still need the to pass and id to the find, how do i get the id ? look lower
+   # routes file, everytime the params hash comes from withing the comments nested path, the router adds
+   # the context type to the params hash, so i access that value by reading from params hash and
+   # accessing the inner value  params[:constraint] out of which [:context_type] is one, and you can pull
+   # the value of a hash inside a hash the way you pull the value of a nested array.  In each context the value is
+   # returned telling if am accesing comments from inside the articles or the proyects, so those are the 2 values i can get
+   # but they are only strings, and are in plural, so i make it singular with singularize and make them constants with
+   # contantize, that way the values become Article and Proyect, the name of the models! so now they are model
+   # names and i call find on them, but i still need the to pass an id to the find,so how do i get the id ? look lower
    # at the context_id method.
    #
    # *finder_options just passes down any option pass to the context_object method and uses it in the find
@@ -97,13 +96,14 @@ class CommentsController < ApplicationController
     params[:constraint][:context_type].singularize.classify.constantize.find( context_id, *finder_options )
   end
 
-  # this here is the action the get the id of the parent model of the polymorphic association, just as the previous
-  # method is catches where is the the comments action been called and used that to build a parent_id symbol and then
-  # uses that to get the value out of the params, becuase the params hash also carries get this value
-  # /articles/:article_id/comments  or /articles/:proyect_id/comments from the route and extracts the corresponding
+  # this here is the action the gets the id of the parent model of the polymorphic association, just as the previous
+  # method is catches where is the the comments action been called and uses that to build a parent_id symbol and then
+  # uses that to get the value out of the params hash, becuase the params hash also carries this value it the path
+  # /articles/:article_id/comments  or /articles/:proyect_id/comments passed by the route and extracts the corresponding
   # id of the parent in the given path, so what im doing at the end, is using context_type to build the param
-  # :article_id  or :proyect_id depending on the case
-  #
+  # :article_id  or :proyect_id depending on the case and then using that symbol to get the proper params[:article_id] or
+  # params[:proyect_id]
+
   def context_id
     params["#{ params[:constraint][:context_type].singularize }_id"]
   end
