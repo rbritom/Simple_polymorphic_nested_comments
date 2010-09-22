67e8ba638ee96b94792806cd8034a8289f467aeb
Radhames Brito <rbritom@gmail.com>
2010-09-21 20:46:28 -0400

    added loading git and ajax delete

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -5,6 +5,7 @@ $("#comment_parent_id").val($(this).attr("comment_id"));
 
 
 $($("#new_comment").live("submit",function(){
+$(".adding_comment").css("background", "url('/images/create-loader.gif')")
 $.post(this.action,$(this).serialize(),null,'script');
 return false;
 }));
@@ -12,6 +13,7 @@ return false;
 
 $(function() {
   $(".pagination a").live("click", function() {
+    $(".loading_pages").css("background", "url('/images/ajax-loader.gif')")
     $.get(this.href, null, null, "script");
     return false;
   });
